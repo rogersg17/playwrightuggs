a69feef551f2d59fc7b125b4ac5d97bdc924893a
rogersg17 <rogers.garry@gmail.com>
2023-01-08 22:26:23 +0000

    Edit test to check page title

--- tests/uggStockCheck.spec.ts
+++ tests/uggStockCheck.spec.ts
@@ -2,6 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('UggTest', async ({ page }) => {
   await page.goto('https://www.ugg.com/eu/en/ie/');
+  await expect(page).toHaveTitle(/Ugg/i);
 // await page.getByRole('searchbox', { name: 'Search' }).fill('Women\'s Classic Mini II Boot');
 //  await page.getByRole('link', { name: 'Women\'s Classic Mini II Boot' }).nth(2).click();
 //  await expect(page.getByRole('heading', { name: 'Classic Mini II Boot' })).toBeVisible();
