0f1e4391b98c7c27a18ec93d18b7ca3e0f34cc72
rogersg17 <rogers.garry@gmail.com>
2023-01-29 23:26:22 +0000

    Updated test

--- tests/daft.spec.ts
+++ tests/daft.spec.ts
@@ -17,21 +17,21 @@ test('Login to Daft', async ({ page }) => {
   await expect(page).toHaveURL(/www.daft.ie/);
 
   // Select For Sale from 700000 Sorted by Most Recent
-  await page.getByTestId('quick-link-0').click();
-  await page.getByTestId('salePrice').click();
-  await page.getByTestId('salePriceFrom').selectOption('700000');
-  await page.getByTestId('salePriceMenu').getByTestId('filter-done-button').click();
-  await page.getByTestId('sort').selectOption('publishDateDesc');
+  //await page.getByTestId('quick-link-0').click();
+  //await page.getByTestId('salePrice').click();
+  //await page.getByTestId('salePriceFrom').selectOption('700000');
+  //await page.getByTestId('salePriceMenu').getByTestId('filter-done-button').click();
+  //await page.getByTestId('sort').selectOption('publishDateDesc');
 
   //click SignIn
-  await page.getByTestId('top-level-active-nav-link').click();
+  //await page.getByTestId('top-level-active-nav-link').click();
 
   // SignIn and SignOut
-  await page.getByLabel('Email or username').fill(process.env.USERNAME!);
-  await page.getByLabel('Password').fill(process.env.PASSWORD!);
-  await page.getByRole('button', { name: 'SIGN IN' }).click();
-  await page.getByTestId('nav-item-mydaft').getByTestId('top-level-nav-item').click();
-  await page.getByRole('link', { name: 'Sign out' }).click();
-  await page.getByRole('button', { name: 'Logout' }).click();
-  await page.close();
+  //await page.getByLabel('Email or username').fill(process.env.USERNAME!);
+  //await page.getByLabel('Password').fill(process.env.PASSWORD!);
+  //await page.getByRole('button', { name: 'SIGN IN' }).click();
+  //await page.getByTestId('nav-item-mydaft').getByTestId('top-level-nav-item').click();
+  //await page.getByRole('link', { name: 'Sign out' }).click();
+  //await page.getByRole('button', { name: 'Logout' }).click();
+  //await page.close();
 });
